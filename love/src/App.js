2320261f24ee36a7a1378564082f00f3aa82187e
filbost <luckozhev.roma@yandex.ru>
2022-12-12 09:11:25 +0500

    initial commit

---
+++ love/src/App.js
@@ -0,0 +1,44 @@
+import  "../images/dribbles.png";
+
+function App() {
+  return (
+    <>
+    
+    <div class="nav-container">
+        <header>
+            <a href="index.html" class="logo">Roman Lukozhev</a>
+    
+            <nav>
+               <ul>
+                <li><a href="#">Don't look</a></li>
+                <li><a href="#">At my</a></li>
+                <li><a href="#">Project,but wait..</a></li>
+                <li><a href="#">Which demon did Makim choose?</a></li>
+               </ul> 
+            </nav>
+        </header>
+        <div class="social-header">
+            <ul>
+                <li><a href="#"><img src="../images/dribbles.png" alt="dribbles"/></a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="hero">
+        <div class="content">
+            <h1>111</h1>
+            <div class="meet">
+                <span>❤️</span>
+                <p>1111</p>
+            </div>
+            <img src="../images/scroll.png" alt="scroll"/>
+        </div>
+    </div>
+    
+    </>
+    
+    
+  );
+}
+
+export default App;
